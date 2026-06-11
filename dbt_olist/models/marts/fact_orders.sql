8af5b142169699da1caf4f578d03301fb8499de9
balkis293-sg <balkis293@gmail.com>
2026-06-12 01:05:40 +0800

    updated ELT

--- dbt_olist/models/marts/fact_orders.sql
+++ dbt_olist/models/marts/fact_orders.sql
@@ -11,23 +11,19 @@ staging_orders AS (
 )
 
 SELECT
-    -- Primary Key for the Fact Table
-    i.order_item_id AS order_item_sk,    
-    
-    -- Foreign Keys connecting to your Dimensions
-    i.order_id AS order_key,             
-    i.product_id AS product_key,         
-    i.seller_id AS seller_key,           
-    o.customer_id AS customer_key,       
-    
-    -- Formatting the timestamp into a string 'YYYYMMDD' to match dim_date
+    CONCAT(i.order_id, '-', CAST(i.order_item_id AS STRING)) AS order_item_sk,
+
+    i.order_id AS order_key,
+    i.product_id AS product_key,
+    i.seller_id AS seller_key,
+    o.customer_id AS customer_key,
+
     CAST(FORMAT_DATE('%Y%m%d', DATE(o.order_purchase_timestamp)) AS STRING) AS date_key,
-    
-    -- Your Facts (Measurable Numbers)
+
     i.price,
     i.freight_value
 
 FROM staging_order_items i
--- We join the orders table to bring in the customer and date information!
-LEFT JOIN staging_orders o 
+LEFT JOIN staging_orders o
     ON i.order_id = o.order_id
+WHERE o.order_id IS NOT NULL
